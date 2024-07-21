aa78272a60a3d0da9c8f92686897274f56f5f04a
Chetna Lilhare <chetnalilhare7@gmail.com>
2024-07-21 22:07:46 +0530

    Update mongoose.ts

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,31 +1,29 @@
 import mongoose from 'mongoose';
 
-let isConnected = false; // Track connection status
-
-export const connectToDB = async () => {
-  mongoose.set('strictQuery', true);
-  
-  if (!process.env.MONGODB_URI) {
-    console.error('MONGODB_URI is not defined');
-    throw new Error('MONGODB_URI is not defined');
-  }
-
-  if (isConnected) {
-    console.log('=> using existing database connection');
-    return;
-  }
-
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Adjust as needed
-      socketTimeoutMS: 45000, // Adjust as needed
-    });
-    isConnected = true;
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    throw new Error('MongoDB connection error');
-  }
-};
+ let isConnected = false; // Track connection status
+ 
+ export const connectToDB = async () => {
+   mongoose.set('strictQuery', true);
+ 
+   if (!process.env.MONGODB_URI) {
+     console.error('MONGODB_URI is not defined');
+     throw new Error('MONGODB_URI is not defined');
+   }
+ 
+   if (isConnected) {
+     console.log('=> using existing database connection');
+     return;
+   }
+ 
+   try {
+     await mongoose.connect(process.env.MONGODB_URI, {
+       serverSelectionTimeoutMS: 5000, // Adjust as needed
+       socketTimeoutMS: 45000, // Adjust as needed
+     });
+     isConnected = true;
+     console.log('MongoDB Connected');
+   } catch (error) {
+     console.error('MongoDB connection error:', error);
+     throw new Error('MongoDB connection error');
+   }
+ };
