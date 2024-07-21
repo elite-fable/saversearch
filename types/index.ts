07131747a6c0e4e72aabd6dbf4ecc58996412604
Chetna Lilhare <chetnalilhare7@gmail.com>
2024-07-21 17:33:09 +0530

    first commit

---
+++ types/index.ts
@@ -0,0 +1,44 @@
+export type PriceHistoryItem = {
+  price: number;
+};
+
+export type User = {
+  email: string;
+};
+
+export type Product = {
+  _id?: string; // Optional ID
+  url: string;
+  currency: string;
+  image: string;
+  title: string;
+  currentPrice: number;
+  originalPrice: number;
+  priceHistory: PriceHistoryItem[]; // Empty array by default
+  highestPrice: number;
+  lowestPrice: number;
+  averagePrice: number;
+  discountRate: number;
+  description: string;
+  category: string;
+  reviewsCount: number;
+  stars: number;
+  isOutOfStock: boolean; // Use lowercase `boolean`
+  users?: User[]; // Optional array of users
+};
+
+export type NotificationType =
+  | "WELCOME"
+  | "CHANGE_OF_STOCK"
+  | "LOWEST_PRICE"
+  | "THRESHOLD_MET";
+
+export type EmailContent = {
+  subject: string;
+  body: string;
+};
+
+export type EmailProductInfo = {
+  title: string;
+  url: string;
+};
