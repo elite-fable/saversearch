7ba037539bad0eb8b2de733f4c4ee2ddda52fe4b
Chetna Lilhare <chetnalilhare7@gmail.com>
2024-07-21 22:34:50 +0530

    Update mongoose.ts

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,29 +1,19 @@
 import mongoose from 'mongoose';
 
- let isConnected = false; // Track connection status
- 
- export const connectToDB = async () => {
-   mongoose.set('strictQuery', true);
- 
-   if (!process.env.MONGODB_URI) {
-     console.error('MONGODB_URI is not defined');
-     throw new Error('MONGODB_URI is not defined');
-   }
- 
-   if (isConnected) {
-     console.log('=> using existing database connection');
-     return;
-   }
- 
-   try {
-     await mongoose.connect(process.env.MONGODB_URI, {
-       serverSelectionTimeoutMS: 5000, // Adjust as needed
-       socketTimeoutMS: 45000, // Adjust as needed
-     });
-     isConnected = true;
-     console.log('MongoDB Connected');
-   } catch (error) {
-     console.error('MongoDB connection error:', error);
-     throw new Error('MongoDB connection error');
-   }
- };
+let isConnected=false; //check connection status
+
+export const connectToDB=async ()=>{
+ mongoose.set('strictQuery',true);
+ if(!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
+
+ if(isConnected) return console.log('=>using existing database connection');
+
+ try{
+    await mongoose.connect(process.env.MONGODB_URI);
+    isConnected=true;
+    console.log('MongoDB Connected');
+ }
+ catch(error){
+    console.log(error)
+ }
+} 
